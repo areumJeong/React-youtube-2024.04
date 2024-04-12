1d4716a9aca1c2754a1f220b5a637db91daf2335
areumJeong <add5833@naver.com>
2024-04-12 15:50:33 +0900

    Refactoring

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,43 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import Grid from '@mui/material/Grid';
 import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import VideoCard from "../components/VideoCard";
 
-const keywordUri = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet&q=`;
-const popularUri = `https://youtube.googleapis.com/youtube/v3/videos?chart=mostPopular&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet`;
+import { useVideo } from '../api/youtube';
 
 export default function Videos() {
   const { keyword } = useParams();
-  const {isLoading, error, data: videos} = useQuery({
-    queryKey: ['videos', keyword],
-    queryFn: async () => {
-      const uri = keyword ? keywordUri + keyword : popularUri;
-      return axios
-              .get(`/data/${keyword ? 'search' : 'popular'}.json`)
-              // .get(uri)
-              .then(res => res.data.items);
-    },
-    staleTime: 1000 * 60 * 1,       // 1분, ms 단위로 지정할 수 있음
-  });
-  // useEffect(() => {
-  //   axios.get(`/data/${keyword ? 'search' : 'popular'}.json`)
-  //     .then(res => {
-  //       setVideos(res.data.items);
-  //       console.log(videos);
-  //     });
-  // }, [keyword]);
+  const { isLoading, error, videos } = useVideo(keyword);
+
   return (
     <>
-      {/* <div>Videos {keyword ? `${keyword}로 검색` : 'Hot Trend'}</div> */}
       {isLoading && <p><HourglassTopIcon /> Loading...</p>}
       {error && <p><WarningAmberIcon /> Something is wrong!!!</p>}
       {videos && (
-        <ul>
-          {videos.map(video => <VideoCard video={video} />)}
-        </ul>
+        <Grid container spacing={1}>
+          {videos.map(video => (
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <VideoCard video={video} />
+            </Grid>
+          ))}
+        </Grid>
       )}
     </>
   )
